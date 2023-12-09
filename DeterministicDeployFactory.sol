7f7ca3ff30ade1ac57fa7f3689988d540a06e52d
Quentin Adam <quentinadam@gmail.com>
2023-12-09 18:52:17 -0300

    Update DeterministicDeployFactory

--- DeterministicDeployFactory.sol
+++ DeterministicDeployFactory.sol
@@ -1,17 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+error DeployError();
+
 contract DeterministicDeployFactory {
     event Deploy(address addr);
 
-    function deploy(bytes memory bytecode, uint _salt) external {
-        address addr;
+    function deploy(bytes memory bytecode, uint256 salt) external payable returns (address addr) {
+        uint256 value = msg.value;
         assembly {
-            addr := create2(0, add(bytecode, 0x20), mload(bytecode), _salt)
-            if iszero(extcodesize(addr)) {
-                revert(0, 0)
-            }
+            addr := create2(value, add(0x20, bytecode), mload(bytecode), salt)
+        }
+        if (addr == address(0)) {
+            revert DeployError();
         }
         emit Deploy(addr);
+    }
+
+    function getAddress(bytes memory bytecode, uint256 salt) external view returns (address addr) {
+        addr = address(uint160(uint256(keccak256(abi.encodePacked(uint8(0xff), this, salt, keccak256(bytecode))))));
     }
 }
